caf5883f1014e9fe32f8ffc3a4030bc513bc06b1
ch-epfl-282634 <ch-epfl-282634@noto.epfl.ch>
2021-05-09 14:41:03 +0200

    Split and refactor of the code

--- sportbot.py
+++ sportbot.py
@@ -1,83 +1,70 @@
-from pywikiapi import Site, ApiError
-import re
+import wikipast as wp
+import text_gen as tg
 
-site = Site('http://wikipast.epfl.ch/wikipast/api.php') # Définition de l'adresse de l'API
-site.no_ssl = True # Désactivation du https, car pas activé sur wikipast
-site.login("Zouaoui@Che", "c6thfl7oqrpgrlbpp2oa151jt6vl2mck") # Login du bot
+from sys import argv, exit
+import pandas as pd
+from os.path import exists
 
-
-def get_wiki_text(page, section=None):
-    result = site('parse', page=page, prop=['wikitext'], section=section)
-    return result['parse']['wikitext']
-
-
-def input_character(data):
+def parse_arguments(args):
+    input_file = ""
+    output_pre_process = ""
     
-    for page, text in data.items():
-        
-        fullText = ""
-        
-        for line in text:
-            fullText += line + '\n'
-
-        try:
-            site('edit', title = page, text="".join(fullText), token=site.token(), createonly=True)
-
-        except ApiError as err:
-            if err.data['code'] == 'articleexists':
-                print("The page \"",page,"\" already exist --> modification of the existing page")
-                sort_year(page, text, fullText)
-                return
-
-        except Exception as err:
-            print('Error :', err)
-
-        
-        
-def sort_year(page_name,text, fullText):
-    old_text = get_wiki_text(page_name)
-
-    # When page is empty, just add content
-    if not old_text:
-        site('edit', title=page_name, text=fullText, token=site.token())
-        return
+    valid_parameter = ["--preprocessing", "--output-file-preprocessing", "--input-file"]
+    need_pre_processing = False
+    get_input_file = False
     
-    test_string = old_text.split("\n")
-
-    test_string = [x for x in test_string if x.strip().startswith("*")]
-    # Page does not contain any valid datafication entries
-    if not test_string:
-        site('edit', title=page_name, text=fullText, token=site.token())
-        return
-    
-    for ourLine in text:
-        
-        year = int(ourLine[4:8])
-        temp = [None] * len(test_string)
-        res = [None] * len(test_string)
-        foo = False
-
-        for i in range(len(test_string)):
-            temp[i] = re.findall(r'\d+', test_string[i])
-            res[i] = list(map(int, temp[i]))
-        current_year = 0
-
-        for i in range(len(res)):
-            if year <= res[i][0]:
-                current_year = i
-                foo = True
-                break
-            current_year = i
-
-        previous_line = test_string[current_year]
-
-        if (current_year >= len(res) - 1) and (not foo):  # add after, append
-            old_text = old_text.replace(previous_line, previous_line + '\n' + ourLine, 1)
-        else:
-            old_text = old_text.replace(previous_line, ourLine + '\n' + previous_line, 1)
+    if len(args)==1:
+        print("More than one parameter is needed")
+        exit(1)
+    elif len(args)%2!=1:
+        print("An odd number of parameters is required")
+        exit(1)
+    else:
+        for i in range(1, len(args),2):
+            arg =args[i]
+            val = args[i+1]
             
-    site('edit', title=page_name, text=old_text, token=site.token())
+            if(arg not in valid_parameter):
+                print("Invalid parameter name")
+                exit(1)
+            if(arg == "--preprocessing"):
+                if val == "1":
+                    need_pre_processing = True
+                elif val == "0":
+                    need_pre_processing = False
+                else:
+                    print("An incorrect parameter value has been encountered for parameter --preprocessing")
+                    exit(1)
+            elif (arg ==  "--output-file-preprocessing"):
+                output_pre_process = val
+                
+            elif (arg == "--input-file"):
+                if not exists(val): 
+                    print("The input file doesn't exist")
+                    exit(1)
+                
+                input_file = val
+                get_input_file = True
+                
+        if not get_input_file:
+            print("The --input-file parameter is required")
+            exit(1)
+            
+    return need_pre_processing, input_file ,output_pre_process
+
+def main(argv):
+    need_pre_processing, input_file, output_pre_process = parse_arguments(argv)
     
+    if need_pre_processing:
+        if output_pre_process == "":
+            out_file="preprocess-"+input_file
+        else:
+            out_file = output_pre_process
+    #    preprocess()
+       
+    load_file = out_file if need_pre_processing else input_file
+    data = tg.generate_all_lines(load_file)
+    # wk.import_data(data)
     
-                                     
-                        
+if __name__ == "__main__":
+    main(argv)
---
+++ wikipast.py
@@ -0,0 +1,80 @@
+from pywikiapi import Site, ApiError
+import re
+site = Site('http://wikipast.epfl.ch/wikipast/api.php') # Définition de l'adresse de l'API
+site.no_ssl = True # Désactivation du https, car pas activé sur wikipast
+site.login("Zouaoui@Che", "c6thfl7oqrpgrlbpp2oa151jt6vl2mck") # Login du bot
+
+
+def get_wiki_text(page, section=None):
+    result = site('parse', page=page, prop=['wikitext'], section=section)
+    return result['parse']['wikitext']
+
+
+def input_character(data):
+    
+    for page, text in data.items():
+        
+        fullText = ""
+        
+        for line in text:
+            fullText += line + '\n'
+
+        try:
+            site('edit', title = page, text="".join(fullText), token=site.token(), createonly=True)
+
+        except ApiError as err:
+            if err.data['code'] == 'articleexists':
+                print("The page \"",page,"\" already exist --> modification of the existing page")
+                sort_year(page, text, fullText)
+                return
+
+        except Exception as err:
+            print('Error :', err)
+
+        
+        
+def sort_year(page_name,text, fullText):
+    old_text = get_wiki_text(page_name)
+
+    # When page is empty, just add content
+    if not old_text:
+        site('edit', title=page_name, text=fullText, token=site.token())
+        return
+    
+    test_string = old_text.split("\n")
+
+    test_string = [x for x in test_string if x.strip().startswith("*")]
+    # Page does not contain any valid datafication entries
+    if not test_string:
+        site('edit', title=page_name, text=fullText, token=site.token())
+        return
+    
+    for ourLine in text:
+        
+        year = int(ourLine[4:8])
+        temp = [None] * len(test_string)
+        res = [None] * len(test_string)
+        foo = False
+
+        for i in range(len(test_string)):
+            temp[i] = re.findall(r'\d+', test_string[i])
+            res[i] = list(map(int, temp[i]))
+        current_year = 0
+
+        for i in range(len(res)):
+            if year <= res[i][0]:
+                current_year = i
+                foo = True
+                break
+            current_year = i
+
+        previous_line = test_string[current_year]
+
+        if (current_year >= len(res) - 1) and (not foo):  # add after, append
+            old_text = old_text.replace(previous_line, previous_line + '\n' + ourLine, 1)
+        else:
+            old_text = old_text.replace(previous_line, ourLine + '\n' + previous_line, 1)
+            
+    site('edit', title=page_name, text=old_text, token=site.token())
+    
+    
